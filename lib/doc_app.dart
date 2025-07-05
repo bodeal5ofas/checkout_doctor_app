9844a239e8b09b7baf28e9a5d7cf34418e19121f
bodeal5ofa4 <bolaraafat2003@gmail.com>
2025-07-05 16:48:50 +0300

    finish flavor and init cicd

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,5 +1,6 @@
 import 'package:checkout_doctor_app/core/routing/my_routes.dart';
 import 'package:checkout_doctor_app/core/themeing/colors.dart';
+import 'package:checkout_doctor_app/core/themeing/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -12,15 +13,21 @@ class DocApp extends StatelessWidget {
       designSize: Size(375, 812),
       splitScreenMode: true,
       minTextAdapt: true,
-      child: MaterialApp.router(
+      builder: (context, child) {
+        TextStyles();
+        return MaterialApp.router(
         routerConfig: MyRoutes.router,
         title: 'Doc App',
+        builder: (context, child) => MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaler:TextScaler.linear(1.0) ),
+              child: child!,
+            ),
         theme: ThemeData(
             primaryColor: MyColors.mainBlue,
             scaffoldBackgroundColor: Colors.white),
         debugShowCheckedModeBanner: false,
 //        initialRoute: MyRoutes.onBoardingScreen,
-      ),
+      );}
     );
   }
 }
---
+++ lib/main_development.dart
@@ -0,0 +1,16 @@
+import 'package:checkout_doctor_app/core/dependces_injection/dependces_injection.dart';
+import 'package:checkout_doctor_app/core/routing/my_routes.dart';
+import 'package:checkout_doctor_app/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+// GoRouter router=;
+//fake update
+  setUpGitIt();
+  //not fix
+  runApp(const DocApp());
+}
